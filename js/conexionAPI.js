b5f19b1b260c3e1a66fcba106176fdb8584fb264
Yaggi <yaggim@gmail.com>
2024-06-17 01:02:37 -0300

    Se agrego consumo de get productos

--- js/conexionAPI.js
+++ js/conexionAPI.js
@@ -1,15 +1,32 @@
 // conexionAPI.js
+
 const apiUrl = 'https://backgutzy3d.onrender.com/api/productos'; // URL de tu API
 
 export async function articulos() {
   try {
     const response = await fetch(apiUrl);
+    
     if (!response.ok) {
       throw new Error('Error al obtener productos');
     }
-    const productos = await response.json();
-    return productos;
+    
+    const articulosJSON = await response.json();
+    const articulosAdecuados = adaptarFormato(articulosJSON); // Adaptamos el formato recibido
+    
+    return articulosAdecuados;
   } catch (error) {
     console.error('Error:', error);
+    throw error; // Re-lanzamos el error para manejarlo en el componente que llame a esta función
   }
+}
+
+// Función para adaptar el formato de los productos
+function adaptarFormato(productos) {
+  return productos.map(producto => ({
+    id: producto.Id_Producto,
+    nombre: producto.Nombre,
+    descripcion: producto.Descripcion,
+    img: `../assets/images/${producto.Imagen}`,
+    precio: producto.Precio
+  }));
 }
