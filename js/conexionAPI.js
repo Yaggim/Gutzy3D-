355949f3caa396133da8a5258be817981270230d
Yaggi <yaggim@gmail.com>
2024-06-16 21:51:27 -0300

    Se agrego consumo de get product

--- js/conexionAPI.js
+++ js/conexionAPI.js
@@ -1,7 +1,15 @@
-const urlJSON = '../db/articulos.json';
+// conexionAPI.js
+const apiUrl = 'https://backgutzy3d.onrender.com/api/productos'; // URL de tu API
 
-export const articulos = async () => {
-    const articulos = await fetch(urlJSON);
-    const articulosJSON = await articulos.json();
-    return articulosJSON;
-};
+export async function articulos() {
+  try {
+    const response = await fetch(apiUrl);
+    if (!response.ok) {
+      throw new Error('Error al obtener productos');
+    }
+    const productos = await response.json();
+    return productos;
+  } catch (error) {
+    console.error('Error:', error);
+  }
+}
